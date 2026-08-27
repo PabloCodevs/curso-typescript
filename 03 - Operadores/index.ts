b69c9223128d52f0f2ba843031e984368a45226a
PabloCodevs <pcavero.dev@gmail.com>
2026-08-27 20:45:04 +0200

    feat: operadores aritméticos, comparativos y lógicos

---
+++ 03 - Operadores/index.ts
@@ -0,0 +1,16 @@
+// Operadores aritmeticos
+console.log(1 + 1);
+console.log(1 - 1);
+console.log(10 * 2);
+console.log(10 / 2);
+
+// Operadores comparativos
+console.log(4 == 4); // Comparamos si 4 es igual a 4
+console.log(4 < 5); // Comparamos que 4 es menor que 5
+console.log(4 >= 5); // Comparamos que 4 es mayor o igual a 5
+
+// Operadores lógicos
+console.log(true || true); // True
+console.log(true || false); // False
+console.log(false || true) // False
+console.log(false || false) // False
